6c2d7c8755f1380b630895de7cc5eaf3ee14359e
isudderth <ian.sudderth@gmail.com>
2020-08-05 22:10:55 -0400

    did some basic pg stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use tokio_postgres::{NoTls, Error};
+use tokio::prelude::*;
+use postgres_types::FromSql;
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+
+    println!("connecting");
+
+    let (client, connection) =
+        tokio_postgres::connect(
+            "host=192.168.99.104 port=30004 user=postgres password=postgres",
+            NoTls)
+            .await?;
+
+    println!("connected");
+
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+
+
+    println!("querrying");
+
+    let rows = client.query("SELECT * FROM public.stuff", &[]).await?;
+
+    println!("querried");
+
+    let key: i32 = rows[0].get(0);
+    let value: &str = rows[0].get(1);
+    println!("{} : {}", key, value);
+
+    Ok(())
 }
